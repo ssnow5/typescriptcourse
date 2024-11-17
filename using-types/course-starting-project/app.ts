f0a10097455d008eaaf04b04a49264c6939d64d0
ssnow5 <ssnow5@yahoo.com>
2024-11-17 20:34:55 +0800

    2.29

--- using-types/course-starting-project/app.ts
+++ using-types/course-starting-project/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number) {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 // let someValue: undefined;
@@ -17,3 +22,8 @@ combinedValues = add;
 // combinedValues = 5;
 
 console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+  return result;
+});
