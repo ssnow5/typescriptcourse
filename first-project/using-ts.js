18bc21b51c34904ffdd07fefadc97ea9b709f055
ssnow5 <ssnow5@yahoo.com>
2024-11-17 16:33:54 +0800

    V4

--- first-project/using-ts.js
+++ first-project/using-ts.js
@@ -1,14 +1,14 @@
-var button = document.querySelector('button');
-var input1 = document.getElementById('num1');
-var input2 = document.getElementById('num2');
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
 function add(num1, num2) {
     // if (typeof num1 === 'number' && typeof num2 === 'number') {
     //   return num1 + num2;
     // } else {
     //   return +num1 + +num2;
     // }
-    return num1 + num2;
+    return num1 + num2 + 0;
 }
-button.addEventListener('click', function () {
+button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
--- first-project/using-ts.ts
+++ first-project/using-ts.ts
@@ -1,6 +1,6 @@
-const button = document.querySelector('button');
-const input1 = document.getElementById('num1')! as HTMLInputElement;
-const input2 = document.getElementById('num2')! as HTMLInputElement;
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
 
 function add(num1: number, num2: number) {
   // if (typeof num1 === 'number' && typeof num2 === 'number') {
@@ -9,9 +9,9 @@ function add(num1: number, num2: number) {
   //   return +num1 + +num2;
   // }
 
-  return num1 + num2;
+  return num1 + num2 + 0;
 }
 
-button.addEventListener('click', function () {
+button.addEventListener("click", function () {
   console.log(add(+input1.value, +input2.value));
 });
