d617793b10350d9d5cad13e6730e711852d41858
ssnow5 <ssnow5@yahoo.com>
2024-11-17 20:24:53 +0800

    2.28

--- using-types/course-starting-project/app.ts
+++ using-types/course-starting-project/app.ts
@@ -1,29 +1,19 @@
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  return result;
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+function printResult(num: number) {
+  console.log("Result: " + num);
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
+printResult(add(5, 12));
 
-const combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+// let someValue: undefined;
+
+let combinedValues: (x: number, y: number) => number;
+
+combinedValues = add;
+// combinedValues = printResult;
+// combinedValues = 5;
+
+console.log(combinedValues(8, 8));
