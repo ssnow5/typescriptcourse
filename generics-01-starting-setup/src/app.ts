ad7cddc498ecce5d15734468b078c3f85d55a13b
David Tan <ssnow5@yahoo.com>
2024-11-22 08:01:01 +0800

    'V7.96

---
+++ generics-01-starting-setup/src/app.ts
@@ -0,0 +1,12 @@
+// // Code goes here!
+
+// const names: Array<string> = [];
+// names[0].split(' ');
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+
+// promise.then((data) => data.split(' '));
