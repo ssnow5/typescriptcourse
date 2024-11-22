3de3d96d79002cfd05b17db56efb2738af7bd124
ssnow5 <ssnow5@yahoo.com>
2024-11-22 17:30:52 +0800

    V7.193

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -10,3 +10,99 @@
 // });
 
 // promise.then((data) => data.split(' '));
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+// console.log(merge({ name: "Max" }, { age: 30 }));
+
+const mergedObj = merge({ name: "Max", hobbies: ["sports"] }, { age: 30 });
+//const mergedObj2 = merge({ name: "Max" }, 30);
+
+console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+
+  if (element.length === 1) {
+    descriptionText = "Got 1 element";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe(["Sports", "Cooking"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({ name: "Max" }, "name"));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const TextStorage = new DataStorage<string>();
+TextStorage.addItem("Max");
+//TextStorage.addItem(2);
+TextStorage.removeItem("Max");
+
+console.log(TextStorage.getItems());
+
+// const numberStorage = new DataStorage<number>();
+
+// const maxObj = { name: "Max" };
+
+// const objectStorage = new DataStorage<object>();
+// objectStorage.addItem(maxObj);
+// objectStorage.addItem({ name: "Manu" });
+// objectStorage.removeItem(maxObj);
+// console.log(objectStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Max", "Ana"];
+// names.push("Manu");
